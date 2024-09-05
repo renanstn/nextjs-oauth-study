d68b1983818770113ed2b8f49b9e52ccfafb9550
Renan <renan_ota@hotmail.com>
2024-09-05 01:32:00 -0300

    Format code with prettier

--- my-app/cypress/e2e/spec.cy.js
+++ my-app/cypress/e2e/spec.cy.js
@@ -1,23 +1,21 @@
-describe('Basic tests', () => {
-
-  it('should display the correct heading for home page', () => {
-    cy.visit('/');
-    cy.get('h1').should('have.text', 'Home page');
+describe("Basic tests", () => {
+  it("should display the correct heading for home page", () => {
+    cy.visit("/");
+    cy.get("h1").should("have.text", "Home page");
   });
 
-  it('should display the correct heading for sign in page', () => {
-    cy.visit('/sign-in');
-    cy.get('h1').should('have.text', 'SignInPage');
+  it("should display the correct heading for sign in page", () => {
+    cy.visit("/sign-in");
+    cy.get("h1").should("have.text", "SignInPage");
   });
 
-  it('should display the correct heading for sign out page', () => {
-    cy.visit('/sign-out');
-    cy.get('h1').should('have.text', 'SignOutPage');
+  it("should display the correct heading for sign out page", () => {
+    cy.visit("/sign-out");
+    cy.get("h1").should("have.text", "SignOutPage");
   });
 
-  it('should display the correct heading for profile page', () => {
-    cy.visit('/profile');
-    cy.get('div').should('have.text', 'Renan Santana');
+  it("should display the correct heading for profile page", () => {
+    cy.visit("/profile");
+    cy.get("div").should("have.text", "Renan Santana");
   });
-
 });
